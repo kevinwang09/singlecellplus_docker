948aeecbf36543730caf7e7099534dc6b8fd266a
kevinwang09 <kevin.wang@sydney.edu.au>
2019-08-05 19:46:24 +1000

    upd setup of VM for scMerge 1.1.4

--- gseR.R
+++ gseR.R
@@ -25,15 +25,15 @@ vm <- gce_ssh_setup(vm,
                     key.private = "~/.ssh/id_rsa")
 # gce_ssh(vm, "echo foo", username = "rstudio")
 
-names = readr::read_csv("names.csv")
-userGroups = split(names$Username, f = rep(1:2, length.out = nrow(names)))
+##################################################
 
-purrr::map(.x = userGroups$`1`, 
-           .f = ~ gce_rstudio_adduser(instance = vm, username = .x, password = .x))
-
+# names = readr::read_csv("names.csv")
+# userGroups = split(names$Username, f = rep(1:2, length.out = nrow(names)))
+# 
+# purrr::map(.x = userGroups$`1`, 
+#            .f = ~ gce_rstudio_adduser(instance = vm, username = .x, password = .x))
 # gce_rstudio_adduser(instance = vm, username = "kevin", password = "kevin")
-
-# gce_vm_stop(instances = vm)
+##################################################
 ################################################################################################
 zone = "asia-northeast1-a" ## Tokyo server
 gce_global_project(project)
@@ -49,5 +49,5 @@ vm2 <- gce_ssh_setup(vm2,
                     key.pub = "~/.ssh/id_rsa.pub",
                     key.private = "~/.ssh/id_rsa")
 
-purrr::map(.x = userGroups$`2`, 
-           .f = ~ gce_rstudio_adduser(instance = vm2, username = .x, password = .x))
+# purrr::map(.x = userGroups$`2`, 
+#            .f = ~ gce_rstudio_adduser(instance = vm2, username = .x, password = .x))
